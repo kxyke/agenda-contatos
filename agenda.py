9bc29f80e6988ca74b3881686f46db4ad4bd3369
Kayke <kaykeon18@gmail.com>
2025-06-03 19:02:21 -0300

    Limpeza e gitignore configurado

---
+++ agenda.py
@@ -0,0 +1,14 @@
+contatos = []
+while True:
+    print("\nMenu da Agenda de Contatos: ")
+    print("1. Adicionar um contato")
+    print("2. Visualizar a lista de contatos")
+    print("3. Editar um contato")
+    print("3. Editar um contato")
+
+
+    
+
+    escolha = input("Digite a sua escolha: ")
+
+    if escolha == "1":
